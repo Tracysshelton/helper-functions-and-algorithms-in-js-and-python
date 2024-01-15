3dd2f852ec8e8607ae80546b27b171063d34adf2
Tracys <jimi1966@msn.com>
2024-01-15 12:50:22 -0700

    Update helper-functions-and-algorithms.php

--- helper-functions-and-algorithms.php
+++ helper-functions-and-algorithms.php
@@ -1,1 +1,25 @@
+// This is a function that will check for Armstrong numbers
+function isArmstrong($num) {
+    // Convert the number to a string to find the number of digits
+    $numStr = (string)$num;
+    $numDigits = strlen($numStr);
+    
+    // Initialize sum
+    $sum = 0;
+    
+    // Iterate over each digit and add its power to the sum
+    for ($i = 0; $i < $numDigits; $i++) {
+        $digit = (int)$numStr[$i];
+        $sum += pow($digit, $numDigits);
+    }
+    
+    // Check if the sum is equal to the original number
+    return $sum === $num;
+}
 
+$num = 76;
+if (isArmstrong(num)) {
+      echo "$num is a Armstrong number.";
+} else {
+      echo "$num is not a Armstrong number.";
+}
