7c5bd31b4bbc0f39cc50af8f98ba097e659ea629
Tracys <jimi1966@msn.com>
2024-01-17 10:32:02 -0700

    Update advance-algorithms-functions.php

--- advance-algorithms-functions.php
+++ advance-algorithms-functions.php
@@ -1,1 +1,24 @@
+//Bubble sort
 
+class BubbleSort {
+    public static function sort(array &$arr) {
+        $n = count($arr);
+
+        for ($i = 0; $i < $n - 1; $i++) {
+            for ($j = 0; $j < $n - $i - 1; $j++) {
+                if ($arr[$j] > $arr[$j + 1]) {
+                    // Swap $arr[$j] and $arr[$j + 1]
+                    $temp = $arr[$j];
+                    $arr[$j] = $arr[$j + 1];
+                    $arr[$j + 1] = $temp;
+                }
+            }
+        }
+    }
+}
+
+// Example usage:
+$numbers = [64, 34, 25, 12, 22, 11, 90];
+BubbleSort::sort($num);
+
+echo "Sorted array: " . implode(", ", $num);
