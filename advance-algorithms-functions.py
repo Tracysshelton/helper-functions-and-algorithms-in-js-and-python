eec89e7effe1c88f0bd599d2eb5ce8adf1e0a56c
Tracys <jimi1966@msn.com>
2024-01-17 19:12:34 -0700

    Update advance-algorithms-functions.py

--- advance-algorithms-functions.py
+++ advance-algorithms-functions.py
@@ -1,1 +1,21 @@
+# Shellsort algorithm
+
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+
+# Example:
+my_list = [12, 34, 54, 2, 3]
+shell_sort(my_list)
+print("Sorted array:", my_list)
 
